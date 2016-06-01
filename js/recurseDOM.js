9650f97399e70e12e0735f420a534a69c0f9c5f7
kekuso <kekuso808@gmail.com>
2016-05-31 20:08:54 -1000

    Sorted the node list into an array of 20 most occuring elements.

--- js/recurseDOM.js
+++ js/recurseDOM.js
@@ -1,58 +1,30 @@
-//var doc = document.getElementsByTagName("*");
 var docObj = {};
+
+// Recursively traverse the DOM
 function recurseDOM (element) {
   element = element.firstElementChild;
   while(element) {
     // if key already exists, increment it
     if(docObj[element]) {
       docObj[element] = docObj[element] + 1;
-      //console.log("Increment property: " + element);
     }
     // Otherwise create a new key with value 1
     else {
-      //console.log("Creating new property: " + element);
       docObj[element] = 1;
     }
     recurseDOM(element);
     element = element.nextSibling;
   }
-
-
-  // if(!element) {
-  //   return;
-  // }
-  // // if key already exists, increment it
-  // if(docObj[element]) {
-  //   docObj[element] = docObj[element] + 1;
-  //   //console.log("Increment property: " + element);
-  // }
-  // // Otherwise create a new key with value 1
-  // else {
-  //   //console.log("Creating new property: " + element);
-  //   docObj[element] = 1;
-  // }
-
-  // // recurse by starting at the last child
-  // if(element.hasChildNodes()) {
-  //   if (element.firstElementChild.nextSibling) {
-  //     recurseDOM(element.firstElementChild.nextSibling);
-  //   }
-  //   else {
-  //     recurseDOM(element.firstElementChild);
-  //   }
-
-  // }
-  // else if(element.previousSibling) {
-  //   recurseDOM(element.previousSibling);
-  // }
 }
 
-// console.log(doc);
-// var node;
-// for(var i = 0; i < 20; i++) {
-//   node = doc[i].firstElementChild;
-//   if(node) {
-//     console.log(doc[i].firstElementChild);
-//   }
-// }
 recurseDOM(document.firstElementChild);
+
+// Sort the node list into an array of 20 largest elements
+var array = [];
+for(var element in docObj) {
+  array.push([element, docObj[element]]);
+  array.sort(function(a, b) {
+    return (b[1] - a[1]);
+  });
+}
+array = array.slice(0, 21);
